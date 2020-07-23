be5493af5290e928b9fe082cf92489f772e75ee6
Christopher <csandsxd@gmail.com>
2020-07-22 21:46:40 -0400

    postUpdate function

--- bot.js
+++ bot.js
@@ -1,8 +1,22 @@
 const twit = require('twit');
 const config = require('./config.js');
+/*
+# Create a config.js file that follows the following template
+# for the Twitter account you are trying to post to.
+module.exports = {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: '',
+}
+*/
 
-const T = new twit(config);
+const postUpdate = (config, content) => {
+    const T = new twit(config);
 
-T.post('statuses/update', { status: 'hello world' }, (err, data, response) => {
-    console.log(data);
-});
+    T.post('statuses/update', { status: content }, (err, data, response) => {
+        console.log(data);
+    });
+}
+
+postUpdate(config, 'hello world');
